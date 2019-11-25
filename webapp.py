213511f63111e1d84046191bfbdf28b9fe44ab74
ilian001 <ilian001@ucr.edu>
2019-11-25 16:43:12 +0000

    Milestone 2

--- webapp.py
+++ webapp.py
@@ -1,11 +1,22 @@
+from gpiozero import LED
 from flask import *
-
 app = Flask(__name__)
 
+LED_Relay = LED(21) 
+
 @app.route("/")
-def hello():
-    return "Hello, World!"  
+def index():
+    return render_template('index.html') 
+@app.route("/led/<int:state>", methods=['POST'])
+def led(state):
+    if state ==0:
+        LED_Relay.off()
+    elif state == 1:
+        LED_Relay.on()
+    else:
+        return("Error Uknown State")
+    return('',204)
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', debug=True)
-    
+    app.run(host='0.0.0.0', debug=True)
+   
