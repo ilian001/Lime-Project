85ff73e2a9b8d6fcf112e6c00aae776f4ac624f0
ilian001 <ilian001@ucr.edu>
2019-11-25 17:36:03 +0000

    final commit

--- webapp.py
+++ webapp.py
@@ -3,18 +3,22 @@ from flask import *
 app = Flask(__name__)
 
 LED_Relay = LED(21) 
+Buzzer = LED(20)
+Buzzer.on()
 
 @app.route("/")
 def index():
     return render_template('index.html') 
 @app.route("/led/<int:state>", methods=['POST'])
 def led(state):
-    if state ==0:
+    if state == 0:
         LED_Relay.off()
+        Buzzer.on()
     elif state == 1:
         LED_Relay.on()
+        Buzzer.off()
     else:
-        return("Error Uknown State")
+        return("Error Unknown State")
     return('',204)
 
 if __name__ == "__main__":
